c438524e9e26c2f80d8bd10c2c1ee5acc1aacd34
karlosqwer <contatokarlos@hotmail.com>
2024-09-28 07:59:22 -0300

    Projeto finalizado, pronto para alterações

--- scr/javascript/script.js
+++ scr/javascript/script.js
@@ -2,3 +2,11 @@
 document.querySelector('.botao-visita').addEventListener('click', function() {
     window.open('https://github.com/karlosqwer', '_blank');
 });
+// Seleciona o botão de download
+document.getElementById('download-cv').addEventListener('click', function() {
+    // Define o caminho relativo do arquivo PDF
+    const link = document.createElement('a');
+    link.href = 'scr/downloads/CURRICULO-KARLOS.pdf'; // Caminho relativo ao arquivo
+    link.download = 'CURRICULO-KARLOS.pdf'; // Nome do arquivo baixado
+    link.click(); // Simula o clique
+});
