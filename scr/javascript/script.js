c781caff3edaf7dd5fda766d12a51a38a3c8258a
karlosqwer <contatokarlos@hotmail.com>
2024-09-27 22:58:20 -0300

    Conclusão das sections

---
+++ scr/javascript/script.js
@@ -0,0 +1,4 @@
+// Seleciona o botão com a classe "botao-visita"
+document.querySelector('.botao-visita').addEventListener('click', function() {
+    window.open('https://github.com/karlosqwer', '_blank');
+});
