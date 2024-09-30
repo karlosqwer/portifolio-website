550f92b2c6472a0037f22bb421c8302cc108f1b4
karlosqwer <contatokarlos@hotmail.com>
2024-09-30 11:16:51 -0300

    Implementação de JavaScript para adicionar funcionalidade responsiva ao menu de navegação.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,6 @@
-// Seleciona o botão com a classe "botao-visita"
-document.querySelector('.botao-visita').addEventListener('click', function() {
-    window.open('https://github.com/karlosqwer', '_blank');
-});
-// Seleciona o botão de download
-document.getElementById('download-cv').addEventListener('click', function() {
-    // Define o caminho relativo do arquivo PDF
-    const link = document.createElement('a');
-    link.href = 'scr/downloads/CURRICULO-KARLOS.pdf'; // Caminho relativo ao arquivo
-    link.download = 'CURRICULO-KARLOS.pdf'; // Nome do arquivo baixado
-    link.click(); // Simula o clique
-});
+const iconeMenu = document.querySelector('#icone-menu');
+const navLinks = document.querySelector('.nav-links');
+
+iconeMenu.onclick = () => {
+    navLinks.classList.toggle('active');
+};
